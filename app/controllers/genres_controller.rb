df2a5b7d1e5db37105a54f8ba0d2a3ec98b9701d
kpneylon <kenneth_neylon@hotmail.com>
2020-05-12 21:29:10 -0700

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,32 @@
+class GenresController < ApplicationController
+  before_action :set_genre, only: [:show, :edit, :update]
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @genre.update(genre_params(:name))
+    redirect_to @genre
+  end
+
+  private
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
+
+  def set_genre
+    @genre = Genre.find(params[:id])
+  end
+end
